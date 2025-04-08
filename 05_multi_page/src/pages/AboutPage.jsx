08de67a09efde12d03dad88a62a5b79d69811b39
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-08 15:21:21 +0900

    feat: Get URL query parameters using useSearchParams

--- 05_multi_page/src/pages/AboutPage.jsx
+++ 05_multi_page/src/pages/AboutPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useSearchParams } from "react-router-dom";
 const AboutPage = () => {
+    let [query, setQuery] = useSearchParams();
+    // 쿼리의 특정 값을 가져올 수 있다
+    console.log("ddd", query.get("q"));
     return <div>AboutPage</div>;
 };
 
--- 05_multi_page/src/pages/HomePage.jsx
+++ 05_multi_page/src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Link, useNavigate } from "react-router-dom";
 const HomePage = () => {
-    return <div>안녕</div>;
+    const navigate = useNavigate();
+    const goAboutPage = () => {
+        //쿼리 뒤에 있는 값은 url에 영향을 끼치지 않는다
+        navigate("/about?q=pants");
+    };
+    return (
+        <div>
+            <h1>HomePage</h1>
+            <Link to="/contact">Go to contact page</Link>
+            <button onClick={goAboutPage}>go to About page</button>
+        </div>
+    );
 };
 
 export default HomePage;
