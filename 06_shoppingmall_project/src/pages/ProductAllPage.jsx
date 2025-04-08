560d28dfe133e6cf50256b35a9fa5679c2bf02e3
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-09 01:57:19 +0900

    fix: Change fetch URL to use JSON server for deployment

--- 06_shoppingmall_project/src/pages/ProductAllPage.jsx
+++ 06_shoppingmall_project/src/pages/ProductAllPage.jsx
@@ -6,10 +6,12 @@ const ProductAllPage = () => {
     const [productList, setProductList] = useState([]);
 
     const getProducts = async () => {
-        let url = "http://localhost:5000/products/";
+        // let url = "http://localhost:5000/products/";
+
+        let url =
+            "https://my-json-server.typicode.com/Choi-jujuyeon/TESTDATA/products";
         let response = await fetch(url);
         let data = await response.json();
-        // console.log(data);
         setProductList(data);
     };
 
