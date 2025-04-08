9cfe4c6170f9eead8a7193219f89eca4614eaeab
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-08 22:40:30 +0900

    feat: fetch product data with useEffect

--- 06_shoppingmall_project/src/pages/ProductAllPage.jsx
+++ 06_shoppingmall_project/src/pages/ProductAllPage.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ProductAllPage = () => {
+    const getProducts = async () => {
+        let url = "http://localhost:5000/products/";
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+    };
+
+    // API 호출->useEffect 사용*
+    useEffect(() => {
+        //함수 호출
+        getProducts();
+    }, []);
     return (
         <div>
             <h1>전체 상품 페이지</h1>
