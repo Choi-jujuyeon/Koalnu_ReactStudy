9478bdd57e90297360401d27c478eb54fabcc1b7
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-08 13:20:18 +0900

    feat: Apply RESTful rout design with dynamic URL /about/:id

--- 05_multi_page/src/App.jsx
+++ 05_multi_page/src/App.jsx
@@ -4,19 +4,18 @@ import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const App = () => {
     return (
-        // <div>
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<HomePage />}></Route>
                 <Route path="contact" element={<ContactPage />} />
                 <Route path="about" element={<AboutPage />} />
+                <Route path="about/:id" element={<ProductDetailPage />} />
             </Route>
         </Routes>
-
-        // </div>
     );
 };
 
