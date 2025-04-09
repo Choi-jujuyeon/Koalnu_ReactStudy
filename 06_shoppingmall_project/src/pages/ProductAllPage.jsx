8a5aee6b80403cddf423db200bc7858c5930a0d2
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-10 01:29:19 +0900

    feat: Add Loading Spinner

--- 06_shoppingmall_project/src/pages/ProductAllPage.jsx
+++ 06_shoppingmall_project/src/pages/ProductAllPage.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
+import LoadingSpinner from "../components/LoadingSpinner";
 const ProductAllPage = () => {
     //UI에 보여주기 위해서는 -> useState 시용*
     const [productList, setProductList] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const getProducts = async () => {
         // let url = "http://localhost:5000/products/";
 
         let url =
             "https://my-json-server.typicode.com/Choi-jujuyeon/TESTDATA/products";
+        setLoading(true);
         let response = await fetch(url);
         let data = await response.json();
         setProductList(data);
+        setLoading(false);
     };
 
     // API 호출->useEffect 사용*
@@ -21,18 +25,20 @@ const ProductAllPage = () => {
         getProducts();
     }, []);
     return (
-        <Container>
-            {/* <h1>전체 상품 페이지</h1> */}
-            <Row>
-                {productList.map((menu) => (
-                    <ProductCard item={menu} />
-                ))}
-                {/* <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard /> */}
-            </Row>
-        </Container>
+        <>
+            {loading ? (
+                <LoadingSpinner loading={loading} />
+            ) : (
+                <Container>
+                    {/* <h1>전체 상품 페이지</h1> */}
+                    <Row>
+                        {productList.map((menu) => (
+                            <ProductCard item={menu} />
+                        ))}
+                    </Row>
+                </Container>
+            )}
+        </>
     );
 };
 
