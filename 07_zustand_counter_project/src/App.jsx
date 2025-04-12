136e411acdde566651131af50ace809235a330bc
Choi-jujuyeon <chlwndus1216@naver.com>
2025-04-12 22:40:56 +0900

    feat: Use Zustand for shared state management

--- 07_zustand_counter_project/src/App.jsx
+++ 07_zustand_counter_project/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { useState } from "react";
 import CountBox from "./components/CountBox";
+import counterStore from "./stores/counterStore";
 const App = () => {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    const { count, increase, increaseBy } = counterStore();
     return (
         <div>
             <h1>count: {count}</h1>
-            <CountBox count={count} />
+            {/* <CountBox count={count} /> */}
+            <button onClick={increase}>increase</button>
+            <button onClick={() => increaseBy(10)}>10씩 증가!increase</button>
+
+            <CountBox />
         </div>
     );
 };
--- 07_zustand_counter_project/src/components/CountBox.jsx
+++ 07_zustand_counter_project/src/components/CountBox.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import counterStore from "../stores/counterStore";
 
-const CountBox = ({ count }) => {
+const CountBox = () => {
+    const { count } = counterStore();
     return (
         <div>
             <h2>CountBox:{count}</h2>
